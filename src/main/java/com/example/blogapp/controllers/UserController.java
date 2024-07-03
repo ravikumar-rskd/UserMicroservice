db4ec6b819d54ac55f2811e055f8bfab6de03aa0
pilakaravi.rs143@gmail.com <pilakaravi.rs143@gmail.com>
2024-07-03 10:24:46 +0530

    Updated UserContrller with Apioperations

--- src/main/java/com/example/blogapp/controllers/UserController.java
+++ src/main/java/com/example/blogapp/controllers/UserController.java
@@ -2,6 +2,10 @@ package com.example.blogapp.controllers;
 
 import com.example.blogapp.models.User;
 import com.example.blogapp.services.UserService;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,30 +14,36 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
+@Api(tags="User Management")
 public class UserController {
     @Autowired
     private UserService userService;
 
+    @ApiOperation("Create a new User")
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody User user) {
         return ResponseEntity.ok(userService.createUser(user));
     }
 
+    @ApiOperation("Get the User details by UserId")
     @GetMapping("/{userId}")
     public ResponseEntity<User> getUserById(@PathVariable int userId) {
         return ResponseEntity.ok(userService.getUserById(userId));
     }
 
+    @ApiOperation("Get all Users")
     @GetMapping
     public ResponseEntity<List<User>> getAllUsers() {
         return ResponseEntity.ok(userService.getAllUsers());
     }
 
+    @ApiOperation("Update User details by UserId")
     @PutMapping("/{userId}")
     public ResponseEntity<User> updateUser(@PathVariable int userId, @RequestBody User userDetails) {
         return ResponseEntity.ok(userService.updateUser(userId, userDetails));
     }
 
+    @ApiOperation("Delete User by UserId")
     @DeleteMapping("/{userId}")
     public ResponseEntity<?> deleteUser(@PathVariable int userId) {
         userService.deleteUser(userId);
